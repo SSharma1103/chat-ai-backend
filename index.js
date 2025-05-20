a6bca5a19cdda5df1bc7eaca470e7cd5f7ae49ca
shivam sharma <shivamsharma11032009@gmail.com>
2025-05-20 18:45:25 +0530

    error fixed -1

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.use("/first", firstRouter);
 app.use("/user", userRouter);
 
 // Use PORT from .env or fallback to 3000
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3002;
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
 });
